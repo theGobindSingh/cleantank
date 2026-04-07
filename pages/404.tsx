05d917ae27105cf121464df20a69d6703bf75cff
theGobindSingh <thesinghgobind@gmail.com>
2026-04-07 17:11:35 +0000

    Refactor code structure for improved readability and maintainability

---
+++ pages/404.tsx
@@ -0,0 +1,39 @@
+import { css } from "@emotion/react";
+import GlobalLayout from "@layouts/global";
+import { mediaQuery } from "@styles/global";
+import NotFoundVector from "@vectors/404";
+import Head from "next/head";
+
+const styles = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  --_height-var: 2.25rem;
+  padding-top: calc(var(--_height-var) + (2 * 1rem) + 2.5rem);
+  padding-bottom: 2.5rem;
+  ${mediaQuery.nonDesktop} {
+    --_height-var: 1.5rem;
+  }
+`;
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Clean Tank Services | 404 - Page Not Found</title>
+        <meta
+          name="description"
+          content="The page you are looking for does not exist."
+        />
+      </Head>
+      <GlobalLayout>
+        <div css={styles}>
+          <NotFoundVector width="480" height="480" />
+        </div>
+      </GlobalLayout>
+    </>
+  );
+};
+
+export default NotFoundPage;
