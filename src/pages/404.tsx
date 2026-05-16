d524adeadcbab0644723628ee7536793c2e0c929
theGobindSingh <thesinghgobind@gmail.com>
2026-05-16 02:18:30 +0000

    feat: Simplify 404 page layout by removing unnecessary components and head metadata

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,7 @@
 import Button from "@components/button";
 import { css } from "@emotion/react";
-import GlobalLayout from "@layouts/global";
 import { mediaQuery } from "@styles/global";
 import NotFoundVector from "@vectors/404";
-import Head from "next/head";
 
 const styles = css`
   display: flex;
@@ -22,31 +20,20 @@ const styles = css`
 
 const NotFoundPage = () => {
   return (
-    <>
-      <Head>
-        <title>Clean Tank Services | 404 - Page Not Found</title>
-        <meta
-          name="description"
-          content="The page you are looking for does not exist."
-        />
-      </Head>
-      <GlobalLayout>
-        <div css={styles}>
-          <NotFoundVector width="480" height="480" />
-          <Button
-            href="https://storyset.com/web"
-            $variant="text"
-            target="_blank"
-            rel="noopener noreferrer"
-            $size="sm"
-            $textColor={"neutral"}
-            $textColorWeight="400"
-          >
-            Web illustration by Storyset
-          </Button>
-        </div>
-      </GlobalLayout>
-    </>
+    <div css={styles}>
+      <NotFoundVector width="480" height="480" />
+      <Button
+        href="https://storyset.com/web"
+        $variant="text"
+        target="_blank"
+        rel="noopener noreferrer"
+        $size="sm"
+        $textColor={"neutral"}
+        $textColorWeight="400"
+      >
+        Web illustration by Storyset
+      </Button>
+    </div>
   );
 };
 
